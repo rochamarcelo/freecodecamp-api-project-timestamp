3625476784f8886d85f9b6d3c3c877767800b9a3
Marcelo Rocha <mrcodex@msn.com>
2017-01-22 19:48:28 +0000

    Added procfile and port

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('/:literal([a-zA-Z]*)/', function (req, res) {
     var data = parseDate(moment(req.params.literal, 'MMMM DD, YYYY'));
     res.send(JSON.stringify(data));
 })
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Example app listening on port ' + process.env.PORT || 8080 + '!')
 })
