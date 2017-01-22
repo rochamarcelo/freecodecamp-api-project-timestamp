65c020a976f96408ce892570feb1f728e68ce7df
Marcelo Rocha <mrcodex@msn.com>
2017-01-22 18:34:24 +0000

    Added routes / /:unit and /:literal

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 var express = require('express')
+var moment = require('moment')
 var app = express()
 
 app.get('/', function (req, res) {
-    console.log("New request at " + new Date());
-  res.send('Hello World!')
+    var data = {
+        unix: null,
+        natural: null
+    };
+    res.send(JSON.stringify(data));
 })
-
+app.get('/:unix(\\d+)/', function (req, res) {
+    
+    var data = {
+        unix: +req.params.unix,
+        natural: moment.unix(+req.params.unix).format('MMMM DD, YYYY')
+    };
+    res.send(JSON.stringify(data));
+})
+app.get('/:literal([a-zA-Z]*)/', function (req, res) {
+    var date = moment(req.params.literal, 'MMMM DD, YYYY');
+    var data = {
+        unix: date.unix(),
+        natural: req.params.literal
+    };
+    if (!data.unix) {
+        data.natural = null;
+    }
+    res.send(JSON.stringify(data));
+})
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
